a1553eb94163babf8e18985bbc47aaa320ecc2ad
Eoic <karolis.strazdas@protonmail.com>
2024-02-14 23:58:58 +0200

    Add basic nav, header, footer templates.

--- lib/netbots_web/components/layouts.ex
+++ lib/netbots_web/components/layouts.ex
@@ -2,4 +2,14 @@ defmodule NetbotsWeb.Layouts do
   use NetbotsWeb, :html
 
   embed_templates "layouts/*"
+  embed_templates "layouts/shared/*"
+
+  def menu_items() do
+    [
+      "Features",
+      "Documentation",
+      "Login",
+      "Register"
+    ]
+  end
 end
